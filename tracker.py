db72575e589a2a09b39babaed9fc184b29420e07
Pallesen01 <38933700+Pallesen01@users.noreply.github.com>
2020-11-26 01:54:32 +1300

    Get teack data functional

--- tracker.py
+++ tracker.py
@@ -1,4 +1,4 @@
-import requests
+import requests, json
 
 info_file = "api_info.txt"
 f = open(info_file)
@@ -18,10 +18,20 @@ def lastfm_get(payload):
     payload['format'] = 'json'
 
     response = requests.get(url, headers=headers, params=payload)
+    return response.json()
+
+def lastfm_gettracks(user, from_unix, to_unix):
+    payload = {
+        'method': 'user.getweeklytrackchart',
+        'user':user,
+        'from':from_unix,
+        'to':to_unix
+    }
+    response = lastfm_get(payload)
     return response
 
 
-payload = {
-    'method': 'chart.gettopartists',
-}
-print(lastfm_get(payload))
+r = lastfm_gettracks('sparks_of_fire',1606176000 , 1606262400)
+f = open("output.txt", "a")
+f.write(json.dumps(r, sort_keys=True, indent=4))
+f.close()
