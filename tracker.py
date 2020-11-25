1eb06d316ce5b1eb43a4e87394a389b071ce5f39
Pallesen01 <38933700+Pallesen01@users.noreply.github.com>
2020-11-26 02:22:18 +1300

    Added some comments

--- tracker.py
+++ tracker.py
@@ -1,5 +1,6 @@
 import requests, json, datetime
 
+#Setup Variables
 unix_day = 86400
 unix_timezone = 43200
 info_file = "api_info.txt"
@@ -11,6 +12,7 @@ registered_user = f.readline().split(':')[1].strip()
 f.close()
 
 def lastfm_get(payload):
+    """Perfoms a lastfm api request"""
     # define headers and URL
     headers = {'user-agent': registered_user}
     url = 'http://ws.audioscrobbler.com/2.0/'
@@ -23,6 +25,7 @@ def lastfm_get(payload):
     return response.json()
 
 def lastfm_gettracks(user, from_unix, to_unix):
+    """Gets all track data for between givin time"""
     payload = {
         'method': 'user.getweeklytrackchart',
         'user':user,
@@ -33,6 +36,7 @@ def lastfm_gettracks(user, from_unix, to_unix):
     return response
 
 def get_unix_time(year,month,day):
+    """Formats a date to unix time"""
     return int(str((datetime.datetime(year,month,day,0,0).timestamp()) - unix_timezone).split('.')[0])
 
 unixtime = get_unix_time(2020, 10, 31)
@@ -41,6 +45,8 @@ data = lastfm_gettracks('sparks_of_fire',unixtime , unixtime+unix_day)
 
 track_list = data['weeklytrackchart']['track']
 
+
+#Output raw data to file
 f = open("output.txt", "w")
 f.write(json.dumps(data, sort_keys=True, indent=4))
 f.close()
